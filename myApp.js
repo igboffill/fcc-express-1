55f66a851db23000b2ecd92b0a380951a44e5088
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-01-14 14:17:22 -0500

    lessons 8.9.10.11

--- myApp.js
+++ myApp.js
@@ -1,24 +1,47 @@
 require('dotenv').config();
+
+let bodyParser = require("body-parser");
 let express = require('express');
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/', (req, res, next)=>{
-    console.log(req.method + ' ' + req.path + ' - '  + req.ip);
+app.use('/', (req, res, next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
     next();
 })
 
-app.get('/', (req, res)=>{res.sendFile(__dirname + '/views/index.html')});
+app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') });
 
-app.get('/json', (req, res)=>{
-    let msg =  "Hello json";
-    if(process.env.MESSAGE_STYLE === 'uppercase'){
+app.get('/json', (req, res) => {
+    let msg = "Hello json";
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
         msg = msg.toUpperCase();
     }
-    res.json({ "message":  msg})
+    res.json({ "message": msg })
 });
 
+app.get('/now',
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => {
+        res.json({ time: req.time });
+    }
+);
+
+app.get('/:word/echo', (req, res) => {
+    res.json({ echo: req.params.word });
+});
+
+app.route('/name')
+    .get((req, res) => {
+        res.json({ name: req.query.firstname + ' ' + req.query.lastname });
+    })
 
 
 
@@ -55,4 +78,5 @@ app.get('/json', (req, res)=>{
 
 
 
- module.exports = app;
+
+module.exports = app;
