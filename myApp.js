e5ecb67fe8141c7f7b6e43a44928198244f91bbf
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-01-14 08:24:43 -0500

    lessons 4, 5 6

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
-console.log('Hello World');
-app.get('/', (req, res)=>{res.send('Hello Express')});
+
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res)=>{res.sendFile(__dirname + '/views/index.html')});
+app.get('/json', (req, res)=>{res.json({"message": "Hello json"})});
 
 
 
