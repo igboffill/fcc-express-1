dd89a5558c5b5ad3c57a60a98dba4bf79f111f9f
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-01-14 09:00:43 -0500

    lessons 6, 7

--- myApp.js
+++ myApp.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
+
+app.use('/', (req, res, next)=>{
+    console.log(req.method + ' ' + req.path + ' - '  + req.ip);
+    next();
+})
+
 app.get('/', (req, res)=>{res.sendFile(__dirname + '/views/index.html')});
-app.get('/json', (req, res)=>{res.json({"message": "Hello json"})});
+
+app.get('/json', (req, res)=>{
+    let msg =  "Hello json";
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        msg = msg.toUpperCase();
+    }
+    res.json({ "message":  msg})
+});
+
 
 
 
